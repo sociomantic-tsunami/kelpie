9e0eba308667e955b678422e9e1c95b9d314f945
Mathias L. Baumann <mathias.baumann@sociomantic.com>
2017-04-25 14:18:12 +0200

    Update to version 0.21

--- incremental-pr-diffs.meta.js
+++ incremental-pr-diffs.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Github PR Incremental Diffs
-// @version      0.20
+// @version      0.21
 // @namespace    https://tampermonkey.net/
 // @homepage     https://github.com/sociomantic-tsunami/kelpie
 // @supportURL   https://github.com/sociomantic-tsunami/kelpie/issues
